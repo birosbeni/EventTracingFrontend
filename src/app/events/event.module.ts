3b06590b9af2b9397b503d63fa77a37022015805
birosbeni <birosbeni@gmail.com>
2023-07-05 20:19:31 +0200

    Add eslint

--- src/app/events/event.module.ts
+++ src/app/events/event.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { EventsListComponent } from './Components/events-list/events-list.component';
 import { EventDetailsComponent } from './Components/event-details/event-details.component';
 import { EventService } from './service/event.service';
 
 @NgModule({
   declarations: [EventsListComponent, EventDetailsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatExpansionModule],
   exports: [EventsListComponent, EventDetailsComponent],
   providers: [EventService],
 })
